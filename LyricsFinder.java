88efedd12f4606a73591d1c96a6740a4de249ed4
sabbb-a <103955672+sabbb-a@users.noreply.github.com>
2022-04-21 22:48:11 -0400

    Add files via upload

--- LyricsFinder.java
+++ LyricsFinder.java
@@ -5,28 +5,47 @@
  */
 package javaapplication45;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 /**
  *
  * @author sabri
  */
 public class LyricsFinder {
-    public static String TitleNoSpace(String userInput){
-        userInput = userInput.replaceAll(" ", "%20");
-        userInput = userInput.toLowerCase();
-        return userInput;
+    private static final String LYRICSURL = "https://api.lyrics.ovh/v1/";
+    
+    public static String removeSpaces (String userInput){
+        if (userInput.contains(" ")){
+            userInput = userInput.replaceAll(" ", "%20");
+            userInput = userInput.toLowerCase();
+            return userInput;
+        }
+        else{
+            return userInput;
+        }
+    }
+
+    public static String lyricsURLBuilder(String title) throws IOException{
+        return ( LYRICSURL+ SongInfo.getArtist(title) + "/" + removeSpaces(title));
     }
     
-    public static String infoURLBuilder(String title){
-        title = TitleNoSpace(title);
-        return ("https://genius.com/api/search/song?q="+title);
+    public static String reading (String title) throws IOException{
+        URL info = null;
+        try {
+            info = new URL(lyricsURLBuilder(title));
+        } catch (MalformedURLException ex) {}
+        BufferedReader in = new BufferedReader(
+        new InputStreamReader(info.openStream()));
+        return in.readLine();
     }
     
-    public static String lyricsURLBuilder(String artist, String title){
-        return ("https://api.lyrics.ovh/v1/" + artist + "/" + title);
-    }
-    
-    public static String readableLyrics(String rawLyrics){
+    public static String readableLyrics(String title) throws IOException{
+        String rawLyrics = reading(title);
         rawLyrics = rawLyrics.replaceAll("\\{\"lyrics\":\"", "");
         rawLyrics = rawLyrics.replaceAll("\"}", "");
         rawLyrics = rawLyrics.replaceAll(" \\\\n\\\\n\\\\n\\\\n", "\n\n");
@@ -37,6 +56,8 @@ public class LyricsFinder {
         rawLyrics = rawLyrics.replaceAll("\\\\n\\\\n", "\n");
         rawLyrics = rawLyrics.replaceAll(" \\\\n", "\n");
         rawLyrics = rawLyrics.replaceAll("\\\\n", "\n");
+        rawLyrics = rawLyrics.replaceAll("\\\\\"", "\"");
+        rawLyrics = rawLyrics.replaceAll("\\\\r", "");
         return rawLyrics;
     }
 }
--- SongInfo.java
+++ SongInfo.java
@@ -11,25 +11,56 @@ package javaapplication45;
  */
 import java.net.*;
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 public class SongInfo {
-
-    /**
-     * @param args the command line arguments
-     */
+    private static final String INFOURL = "https://genius.com/api/search/song?q=";
     
-    public static String getArtist(String input){
+    public static String removeSpaces (String userInput){
+        if (userInput.contains(" ")){
+            userInput = userInput.replaceAll(" ", "%20");
+            userInput = userInput.toLowerCase();
+            return userInput;
+        }
+        else{
+            return userInput;
+        }
+    }
+
+    public static String getArtist(String title) throws IOException{
+        String input = reading(title);
         input = input.substring((input.indexOf("names\":\"")+8), input.indexOf("\",\"full"));
+        if(input.contains(" (Ft. ")){
+            input = input.substring(0, input.indexOf(" (Ft. "));
+        }
         return input;
     }
-    public static String getTitle(String input){
+    
+    public static String getTitle(String title) throws IOException{
+        String input = reading(title);
         input = input.substring((input.indexOf("\"title\":\"")+9), input.indexOf("\",\"title_with"));
         return input;
     }
-    public static String getApiPath(String input){
+    public static String getApiPath(String title) throws IOException{
+        String input = reading(title);
         input = input.substring((input.indexOf("path\":\"/songs/")+14), input.indexOf("\",\"artist_names"));
         return input;
+    }
+    
+    public static String infoURLBuilder(String title){
+        return (INFOURL + removeSpaces(title));
+    }
+    
+    public static String reading (String title) throws IOException{
+        URL info = null;
+        try {
+            info = new URL(infoURLBuilder(title));
+        } catch (MalformedURLException ex) {}
+        BufferedReader in = new BufferedReader(
+        new InputStreamReader(info.openStream()));
+        return in.readLine();
     }
 }
    
